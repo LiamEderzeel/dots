835edd0282c1ced699965d9e95b4d0dd16f4652d
Liam Ederzeel <liamederzeel@gmail.com>
2025-04-29 14:33:28 +0200

    feat(nixos): change from hyprpaper to swww with rotating wallpapers

--- .nixos/common/pkgs.nix
+++ .nixos/common/pkgs.nix
@@ -38,6 +38,7 @@ let
     networkmanagerapplet
     protonvpn-gui
     postman
+    swww
   ];
   stable = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.

6fc9580236a954992d422f05c0b6923bb77380cc
Liam Ederzeel <liamederzeel@gmail.com>
2025-08-29 16:10:58 +0200

    fix(nixos): mongodb-compass keychain access

--- .nixos/common/pkgs.nix
+++ .nixos/common/pkgs.nix
@@ -15,7 +15,29 @@ let
     spotify
     lazygit
     lazydocker
-    mongodb-compass
+    # mongodb-compass
+    (pkgs.runCommand "mongodb-compass-wrapped" {
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      desktopItem = pkgs.makeDesktopItem {
+        name = "MongoDBCompass";
+        desktopName = "MongoDB Compass";
+        exec = "mongodb-compass-gnome";
+        icon = "mongodb-compass";
+        genericName = "MongoDB GUI";
+        categories = [ "Development" "Database" "Utility" ];
+      };
+      passAsFile = [ "desktopItem" ];
+    } ''
+      mkdir -p $out/bin
+      makeWrapper ${pkgs-unstable.mongodb-compass}/bin/mongodb-compass $out/bin/mongodb-compass-gnome \
+        --set XDG_CURRENT_DESKTOP "GNOME"
+    
+      mkdir -p $out/share/applications
+      cp "$(cat $desktopItemPath)"/share/applications/MongoDBCompass.desktop $out/share/applications/
+    
+      mkdir -p $out/share/icons
+      ln -s ${pkgs-unstable.mongodb-compass}/share/icons/* $out/share/icons/
+    '')
     mongosh
     mongodb-tools
     darktable

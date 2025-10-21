1fb0eecdd4a70db6786ee3456e4cac6ee81a128e
Liam Ederzeel <liamederzeel@gmail.com>
2025-10-21 09:53:00 +0200

    chore(nixos): configure bluetooth

--- .nixos/machine/configuration.nix
+++ .nixos/machine/configuration.nix
@@ -345,4 +345,6 @@
     # Keymapp Flashing rules for the Voyager
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
   '';
+
+  services.blueman.enable = true;
 }

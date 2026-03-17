8c257019f0766c0185887c16983b489030e1eb22
Liam Ederzeel <liamederzeel@gmail.com>
2026-03-17 09:29:41 +0100

    fix: add missing dep

--- .nixos/common/pkgs.nix
+++ .nixos/common/pkgs.nix
@@ -75,6 +75,7 @@ let
     papirus-icon-theme
     bibata-cursors
     glib
+    ripgrep
   ];
   stable = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
